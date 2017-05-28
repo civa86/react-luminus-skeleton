ac2aa35c8b9fc288d12d804eb36e74d6eb237563
Dario Civallero <dario.civallero@gmail.com>
2017-05-28 14:44:30 +0200

    file upload form

--- rest/src/clj/rest/routes/context/general.clj
+++ rest/src/clj/rest/routes/context/general.clj
@@ -2,20 +2,53 @@
   (:require [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
             [clojure.tools.logging :as log]
-            [schema.core :as s]))
+            [rest.db.core :refer [*db*] :as db]
+            [schema.core :as s]
+            [clojure.string :as str]))
+
+(def SHEET [{:label "Full Name" :fields [:firstname :lastname]}
+            {:label "Email" :fields [:email]}])
+
+(defn extract-from-item
+  [item fields]
+  (into [] (->> fields
+                (map #(reduce (fn [m v] (conj m (get item v ""))) [] %))
+                (map #(into [] %))
+                (map #(remove nil? %))
+                (map #(str/join ", " %))
+                ))
+  )
+
+(defn generate-sheet [items struct]
+  (let [header (map #(get % :label) struct)
+        fields (map #(get % :fields []) struct)
+        entries (map #(extract-from-item % fields) items)
+        ]
+
+    (clojure.pprint/pprint [(into [] header) (into [] entries)])
+    )
+
+  )
 
 (def ctx-general
   (context "/" []
 
     (GET "/" []
-      :return   s/Any
-      :summary  ""
+      :return s/Any
+      :summary ""
       (log/info "[API] /")
       (ok {:version (System/getProperty "rest.version")}))
 
+    ;TODO remove...just a test
+    (GET "/download" []
+      :return s/Any
+      :summary ""
+      (generate-sheet (db/get-users) SHEET)
+      (ok {:version (System/getProperty "rest.version")}))
+
     (GET "/*" request
-      :return   s/Any
-      :summary  "Not Matched Api Route"
+      :return s/Any
+      :summary "Not Matched Api Route"
       (log/info "[API] " (get request :path-info) " not found")
       (not-found {:code 404 :error "Resource not found"}))
     ))
